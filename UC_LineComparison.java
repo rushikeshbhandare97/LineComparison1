cb4563d86035106722e21b00d1865926df63ad49
technology-97 <rushikeshbhandare789@gmail.com>
2021-07-14 00:13:43 +0600

    Added:UC_LineComputation is added

---
+++ UC_LineComparison.java
@@ -0,0 +1,23 @@
+public class UC_LineComparison {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to line Comparison problem");
+		
+		int x1 ,x2 ,y1 ,y2;
+		double dis = Math.sqrt(x2-x1)*(x2-x1) +(y2-y1)*(y2-y1);
+		System.out.println("distanceBteween" +"("+x1+", "+y1+"), "+"("+x2+" , "+y2+");"
+		if (x2-x1 == y2-y1)
+			 System.out.println();
+	         System.out.println("x2-x1 equals y2-y1");
+	         System.out.println("x2+x1  not equals y2-y1");
+	         
+	       String str1="x2-x1";
+	       String str2="y2-y1";
+	       
+	       System.out.println(str1.compareTo(str2));
+	}
+
+	
+	}
+
